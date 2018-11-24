06507b98030c4ba8ec843445e58b24bd7198ef4e
Nikitos1409 <45121935+Nikitos1409@users.noreply.github.com>
2018-11-24 17:28:21 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,22 +1,21 @@
-import random
-class Player:
-  def __init__(self,name,straight,hp):
-    self.name=name
-    self.straight=straight
+import random "мы импортируем модуль рандом"
+class Player: "создаём класс player"
+  def __init__(self,name,straight,hp): ""в классе player прописываем его имя,его
+  сила и здоровье""
+    self.name=name "имя"
+    self.straight=straight "сила"
     self.x=0
     self.y=0
-    self.hp=100
-  
+    self.hp=100 "здоровье"
 
-  def say(self, text):
-    print(text)
-  
-  def get_damage(self):
-    self.damage=(((self.straight)**2)/2)*random.uniform(1,2)
+
+  def say(self, text):  "функция для текста"
+    print(text) "выводим текс"
+
+  def get_damage(self): "создаем функцию урона"
+    self.damage=(((self.straight)**2)/2)*random.uniform(1,2) "сдесь формула нанесения урона"
 
     return self.damage
-  
-  def attack(self):
-    return self.get_damage()/2 
-  
 
+  def attack(self): "функция атаки"
+    return self.get_damage()/2 "вычесление атаки"
