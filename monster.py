626005d4b0969b1f8b889e7b3f3245c87fa61f82
Nikitos1409 <45121935+Nikitos1409@users.noreply.github.com>
2018-11-29 14:51:15 +0300

    Add files via upload

--- monster.py
+++ monster.py
@@ -1,18 +1,18 @@
-import random
+import random "вызываем модуль рандом"
 class Monster:
-  def __init__(self,name,type,strenght,hp):  
+  def __init__(self,name,type,strenght,hp):  "сдесь то что состит в классе монстр"
     self.x = 0
     self.y = 0
-    self.name = name  
-    self.type = type
-    self.strenght = strenght
-    self.hp = hp
+    self.name = name  "имя"
+    self.type = type "тип"
+    self.strenght = strenght "сила"
+    self.hp = hp "здоровье"
 
-  def say(self,text):
+  def say(self,text): "функция разговора"
     print(text)
 
-  def get_dmg(self):
-    self.dmg = ((self.strenght**2)/2)*random.uniform(1,2)
+  def get_dmg(self): "получение дамага"
+    self.dmg = ((self.strenght**2)/2)*random.uniform(1,2) "формула получения дамаг"
 
 test = Monster("Zyablik","bat",20,100)
 
