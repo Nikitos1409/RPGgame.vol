be006a58d5670010704146a81d65620d941d0d86
Nikitos1409 <45121935+Nikitos1409@users.noreply.github.com>
2018-12-01 14:04:21 +0300

    Add files via upload

--- fight.py
+++ fight.py
@@ -1,8 +1,9 @@
 class Fight:
   def __init__(self,f1,f2):
-    self.f1 = f1
-    self.f2 = f2
+    self.f1 = f1#взятие статистики персонажа из player.py
+    self.f2 = f2#взятие статистики персонажа из monster.py
 
   def tick(self):
-    self.f1.hp = self.f1.hp - self.f2.attack()
-    self.f2.hp = self.f2.hp - self.f1.attack() 
+    self.f1.hp = self.f1.hp - self.f2.attack()#кол-во ОЗ после получения урона для player
+    self.f2.hp = self.f2.hp - self.f1.attack()#кол-во ОЗ после получения урона для monster 
+
